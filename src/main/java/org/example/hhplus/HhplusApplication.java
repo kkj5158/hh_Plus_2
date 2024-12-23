b61b76d10f61b10551d4d24c3920369a42d899ac
jjk5158 <jjk5158@naver.com>
2024-12-23 10:31:09 +0900

    init

---
+++ src/main/java/org/example/hhplus/HhplusApplication.java
@@ -0,0 +1,13 @@
+package org.example.hhplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HhplusApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/hhplus/HhplusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hhplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
